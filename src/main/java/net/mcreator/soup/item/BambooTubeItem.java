b1ac23fa2a1f0f0be34992b16b0f7c1de4268c8c
AT Studio <98391767+Adenx0@users.noreply.github.com>
2022-10-18 21:20:43 +0800

    bamboo tube

---
+++ src/main/java/net/mcreator/soup/item/BambooTubeItem.java
@@ -0,0 +1,44 @@
+
+package net.mcreator.soup.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.soup.SoupModElements;
+
+@SoupModElements.ModElement.Tag
+public class BambooTubeItem extends SoupModElements.ModElement {
+	@ObjectHolder("soup:bamboo_tube")
+	public static final Item block = null;
+
+	public BambooTubeItem(SoupModElements instance) {
+		super(instance, 8);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(ItemGroup.MISC).maxStackSize(64).rarity(Rarity.COMMON));
+			setRegistryName("bamboo_tube");
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+	}
+}
