985b6dc75178c438dd0c71074e6bed000fff2a6f
AT Studio <98391767+Adenx0@users.noreply.github.com>
2022-10-18 20:50:36 +0800

    fiber

---
+++ src/main/java/net/mcreator/soup/item/FiberItem.java
@@ -0,0 +1,44 @@
+
+package net.mcreator.soup.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.soup.SoupModElements;
+
+@SoupModElements.ModElement.Tag
+public class FiberItem extends SoupModElements.ModElement {
+	@ObjectHolder("soup:fiber")
+	public static final Item block = null;
+
+	public FiberItem(SoupModElements instance) {
+		super(instance, 1);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(ItemGroup.MISC).maxStackSize(64).rarity(Rarity.COMMON));
+			setRegistryName("fiber");
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+	}
+}
