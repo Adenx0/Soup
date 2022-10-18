638cbfc95614778be5b9897bbe4482139ecee0f3
AT Studio <98391767+Adenx0@users.noreply.github.com>
2022-10-18 21:03:45 +0800

    graver

---
+++ src/main/java/net/mcreator/soup/item/GraverItem.java
@@ -0,0 +1,84 @@
+
+package net.mcreator.soup.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.entity.ai.attributes.Attributes;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
+import net.minecraft.entity.ai.attributes.Attribute;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.soup.SoupModElements;
+
+import com.google.common.collect.Multimap;
+import com.google.common.collect.ImmutableMultimap;
+
+@SoupModElements.ModElement.Tag
+public class GraverItem extends SoupModElements.ModElement {
+	@ObjectHolder("soup:graver")
+	public static final Item block = null;
+
+	public GraverItem(SoupModElements instance) {
+		super(instance, 6);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(ItemGroup.MISC).maxDamage(100).rarity(Rarity.COMMON));
+			setRegistryName("graver");
+		}
+
+		@Override
+		public boolean hasContainerItem() {
+			return true;
+		}
+
+		@Override
+		public ItemStack getContainerItem(ItemStack itemstack) {
+			ItemStack retval = new ItemStack(this);
+			retval.setDamage(itemstack.getDamage() + 1);
+			if (retval.getDamage() >= retval.getMaxDamage()) {
+				return ItemStack.EMPTY;
+			}
+			return retval;
+		}
+
+		@Override
+		public boolean isRepairable(ItemStack itemstack) {
+			return false;
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 2;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+
+		@Override
+		public Multimap<Attribute, AttributeModifier> getAttributeModifiers(EquipmentSlotType slot) {
+			if (slot == EquipmentSlotType.MAINHAND) {
+				ImmutableMultimap.Builder<Attribute, AttributeModifier> builder = ImmutableMultimap.builder();
+				builder.putAll(super.getAttributeModifiers(slot));
+				builder.put(Attributes.ATTACK_DAMAGE,
+						new AttributeModifier(ATTACK_DAMAGE_MODIFIER, "Item modifier", (double) 1, AttributeModifier.Operation.ADDITION));
+				builder.put(Attributes.ATTACK_SPEED,
+						new AttributeModifier(ATTACK_SPEED_MODIFIER, "Item modifier", -2.4, AttributeModifier.Operation.ADDITION));
+			}
+			return super.getAttributeModifiers(slot);
+		}
+	}
+}
