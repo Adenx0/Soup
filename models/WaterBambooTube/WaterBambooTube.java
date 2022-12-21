3267c14588a8e59d5e13870277e922dcc050c0f1
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2022-12-21 16:01:49 +0800

    Update WaterBambooTube.java

--- models/WaterBambooTube/WaterBambooTube.java
+++ models/WaterBambooTube/WaterBambooTube.java
@@ -5,6 +5,7 @@
 
 public class custom_model extends EntityModel<Entity> {
 	private final ModelRenderer bottom;
+	private final ModelRenderer wall;
 
 	public custom_model() {
 		textureWidth = 64;
@@ -15,6 +16,10 @@ public class custom_model extends EntityModel<Entity> {
 		bottom.setTextureOffset(0, 11).addBox(-5.0F, -1.0F, -4.0F, 10.0F, 1.0F, 8.0F, 0.0F, false);
 		bottom.setTextureOffset(0, 0).addBox(-4.0F, -1.0F, 4.0F, 8.0F, 1.0F, 1.0F, 0.0F, false);
 		bottom.setTextureOffset(0, 0).addBox(-4.0F, -1.0F, -5.0F, 8.0F, 1.0F, 1.0F, 0.0F, false);
+
+		wall = new ModelRenderer(this);
+		wall.setRotationPoint(0.0F, 24.0F, 0.0F);
+		wall.setTextureOffset(0, 0).addBox(-4.0F, -2.0F, -6.0F, 8.0F, 1.0F, 1.0F, 0.0F, false);
 	}
 
 	@Override
@@ -25,6 +30,7 @@ public class custom_model extends EntityModel<Entity> {
 	@Override
 	public void render(MatrixStack matrixStack, IVertexBuilder buffer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha){
 		bottom.render(matrixStack, buffer, packedLight, packedOverlay, red, green, blue, alpha);
+		wall.render(matrixStack, buffer, packedLight, packedOverlay, red, green, blue, alpha);
 	}
 
 	public void setRotationAngle(ModelRenderer modelRenderer, float x, float y, float z) {
