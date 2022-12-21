80757c58d3ed59b10a22efab8ccd582f327948e1
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2022-12-21 15:07:22 +0800

    Create WaterBambooTube.java

---
+++ models/WaterBambooTube/WaterBambooTube.java
@@ -0,0 +1,35 @@
+// Made with Blockbench 4.5.2
+// Exported for Minecraft version 1.15 - 1.16 with MCP mappings
+// Paste this class into your mod and generate all required imports
+
+
+public class custom_model extends EntityModel<Entity> {
+	private final ModelRenderer bottom;
+
+	public custom_model() {
+		textureWidth = 64;
+		textureHeight = 64;
+
+		bottom = new ModelRenderer(this);
+		bottom.setRotationPoint(0.0F, 24.0F, 0.0F);
+		bottom.setTextureOffset(0, 11).addBox(-5.0F, -1.0F, -4.0F, 10.0F, 1.0F, 8.0F, 0.0F, false);
+		bottom.setTextureOffset(0, 0).addBox(-4.0F, -1.0F, 4.0F, 8.0F, 1.0F, 1.0F, 0.0F, false);
+		bottom.setTextureOffset(0, 0).addBox(-4.0F, -1.0F, -5.0F, 8.0F, 1.0F, 1.0F, 0.0F, false);
+	}
+
+	@Override
+	public void setRotationAngles(Entity entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch){
+		//previously the render function, render code was moved to a method below
+	}
+
+	@Override
+	public void render(MatrixStack matrixStack, IVertexBuilder buffer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha){
+		bottom.render(matrixStack, buffer, packedLight, packedOverlay, red, green, blue, alpha);
+	}
+
+	public void setRotationAngle(ModelRenderer modelRenderer, float x, float y, float z) {
+		modelRenderer.rotateAngleX = x;
+		modelRenderer.rotateAngleY = y;
+		modelRenderer.rotateAngleZ = z;
+	}
+}
