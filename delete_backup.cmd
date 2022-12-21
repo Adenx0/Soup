f9c93dc9108e3658ac8e631744b30113202aa121
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2022-12-21 14:36:50 +0800

    Create delete_backup.cmd

---
+++ delete_backup.cmd
@@ -0,0 +1,5 @@
+@echo off
+del /f /s /q D:\Create\mod_project\Soup\.mcreator\workspaceBackups\*.*
+cd  D:\Create\mod_project\Soup\.mcreator\workspaceBackups
+rd /s /q   D:\Create\mod_project\Soup\.mcreator\workspaceBackups
+del /f /s /q D:\Create\mod_project\Soup\run\logs\*.gz
